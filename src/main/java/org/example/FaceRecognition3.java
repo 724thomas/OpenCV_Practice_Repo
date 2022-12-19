841cdfdacd32446ed1d06c988ba7add2575bd268
WonJoonChoi <724thomas@gmail.com>
2022-12-19 10:29:30 +0900

    Fix

--- src/main/java/org/example/FaceRecognition3.java
+++ src/main/java/org/example/FaceRecognition3.java
@@ -1,52 +1,52 @@
-package org.example;
-
-import org.opencv.core.*;
-import org.opencv.face.FaceRecognizer;
-import org.opencv.face.LBPHFaceRecognizer;
-import org.opencv.objdetect.CascadeClassifier;
-import org.opencv.videoio.VideoCapture;
-
-public class FaceRecognition3 {
-    public static void main(String[] args) {
-        // Load the OpenCV library
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-        // Load the LBPH face recognition model
-        FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
-        faceRecognizer.read("/path/to/lbph_model.xml");
-
-        // Load the Haar cascade classifier for face detection
-        CascadeClassifier faceDetector = new CascadeClassifier("C:/opencv/build/etc/haarcascades/haarcascade_frontalface_default.xml");
-
-        // Capture a video stream from the webcam
-        VideoCapture videoCapture = new VideoCapture(0);
-
-        // Loop through the video frames
-        while (true) {
-            // Read a frame from the video stream
-            Mat frame = new Mat();
-            videoCapture.read(frame);
-
-            // Detect faces in the frame
-            MatOfRect faceDetections = new MatOfRect();
-            faceDetector.detectMultiScale(frame, faceDetections);
-
-            // Loop through the detected faces
-            for (Rect rect : faceDetections.toArray()) {
-                // Crop the detected face from the frame
-                Mat face = new Mat(frame, rect);
-
-                // Compare the detected face with the known face
-                int prediction = faceRecognizer.predict(face);
-
-                if (prediction == knownFaceId) {
-                    // The detected face matches the known face
-                    System.out.println("Match found!");
-                } else {
-                    // The detected face does not match the known face
-                    System.out.println("Match not found.");
-                }
-            }
-        }
-    }
-}
+//package org.example;
+//
+//import org.opencv.core.*;
+//import org.opencv.face.FaceRecognizer;
+//import org.opencv.face.LBPHFaceRecognizer;
+//import org.opencv.objdetect.CascadeClassifier;
+//import org.opencv.videoio.VideoCapture;
+//
+//public class FaceRecognition3 {
+//    public static void main(String[] args) {
+//        // Load the OpenCV library
+//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//
+//        // Load the LBPH face recognition model
+//        FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
+//        faceRecognizer.read("/path/to/lbph_model.xml");
+//
+//        // Load the Haar cascade classifier for face detection
+//        CascadeClassifier faceDetector = new CascadeClassifier("C:/opencv/build/etc/haarcascades/haarcascade_frontalface_default.xml");
+//
+//        // Capture a video stream from the webcam
+//        VideoCapture videoCapture = new VideoCapture(0);
+//
+//        // Loop through the video frames
+//        while (true) {
+//            // Read a frame from the video stream
+//            Mat frame = new Mat();
+//            videoCapture.read(frame);
+//
+//            // Detect faces in the frame
+//            MatOfRect faceDetections = new MatOfRect();
+//            faceDetector.detectMultiScale(frame, faceDetections);
+//
+//            // Loop through the detected faces
+//            for (Rect rect : faceDetections.toArray()) {
+//                // Crop the detected face from the frame
+//                Mat face = new Mat(frame, rect);
+//
+//                // Compare the detected face with the known face
+//                int prediction = faceRecognizer.predict(face);
+//
+//                if (prediction == knownFaceId) {
+//                    // The detected face matches the known face
+//                    System.out.println("Match found!");
+//                } else {
+//                    // The detected face does not match the known face
+//                    System.out.println("Match not found.");
+//                }
+//            }
+//        }
+//    }
+//}
---
+++ src/main/java/org/example/TakePicture.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+public class TakePicture {
+    public static void main(String[] args) {
+        // Load the OpenCV library
+        System.out.println(System.getProperty("java.library.path"));
+        System.setProperty("java.library.path", "C:/opencv/build/java/opencv-460.jar");
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        // Create a new video capture object
+        VideoCapture capture = new VideoCapture(0);
+
+        // Check if the video capture object was created successfully
+        if (!capture.isOpened()) {
+            System.out.println("Error: Could not access webcam.");
+            return;
+        }
+
+        // Create a new matrix to store the video frame
+        Mat frame = new Mat();
+
+        // Read a frame from the video capture object
+        capture.read(frame);
+
+        // Save the frame to a file
+        org.opencv.imgcodecs.Imgcodecs.imwrite("picture.jpg", frame);
+
+        // Release the video capture object
+        capture.release();
+    }
+}
