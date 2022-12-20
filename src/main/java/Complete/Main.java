63d93ec6149877c6cc42b6fe09d15fea7db20740
ThomasChoi <724thomas@gmail.com>
2022-12-20 17:10:20 +0900

    Fix

--- src/main/java/Complete/Main.java
+++ src/main/java/Complete/Main.java
@@ -1,9 +1,14 @@
 package Complete;
 
+import org.opencv.core.Core;
+
 public class Main {
     public static void main(String[] args) {
+        nu.pattern.OpenCV.loadLocally();
+        System.setProperty("java.library.path", "C:/opencv/build/java/opencv-460.jar");
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         FaceRecognition.TakePictureAndDetectFace("Test.jpg");
-        FaceRecognition.DrawRectangleOnPicture("Test.jpg");
-        FaceRecognition.FaceSimilarityVideoAndPicture("Test.jpg");
+//        FaceRecognition.DrawRectangleOnPicture("Test.jpg");
+//        FaceRecognition.FaceSimilarityVideoAndPicture("Test.jpg");
     }
 }
