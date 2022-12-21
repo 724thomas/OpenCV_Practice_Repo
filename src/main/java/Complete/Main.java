00ea73e8883cd9a5901157421c4ed4cff72d0dab
ThomasChoi <724thomas@gmail.com>
2022-12-21 18:56:18 +0900

    Fix

--- src/main/java/Complete/Main.java
+++ src/main/java/Complete/Main.java
@@ -2,9 +2,9 @@ package Complete;
 
 public class Main {
     public static void main(String[] args) {
-        FaceRecognition.TakePictureAndDetectFace("Test.jpg");
-        FaceRecognition.cutOnlyFace("Test.jpg");
-        FaceRecognition.DrawRectangleOnPicture("Test.jpg");
+//        FaceRecognition.TakePictureAndDetectFace("Test.jpg");
+//        FaceRecognition.cutOnlyFace("Test.jpg");
+//        FaceRecognition.DrawRectangleOnPicture("Test.jpg");
         FaceRecognition.FaceSimilarityVideoAndPicture("Test.jpg");
     }
 }
